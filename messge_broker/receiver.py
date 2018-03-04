3ee322025b654ccad4b7d1600af6ec1dea9cfa05
Mohammad Khallaghi <md.khallaghi@gmail.com>
2018-03-04 16:38:49 +0330

    change packages and directories and add license file

---
+++ messge_broker/receiver.py
@@ -0,0 +1,27 @@
+import pika
+import json
+from k8s.client import handle_request
+from message import Message
+
+credentials = pika.PlainCredentials('test', 'test')
+parameters = pika.ConnectionParameters(host='localhost',
+                                       port=5672,
+                                       virtual_host='/',
+                                       credentials=credentials)
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+channel.queue_declare(queue='command', durable=True)
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+    msg = json.loads(body)
+    handle_request(Message(**msg))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(callback,
+                      queue='command')
+
+channel.start_consuming()
