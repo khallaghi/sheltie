7b5a4853f6008135005828b1a787cc509845ac5c
Mohammad Khallaghi <md.khallaghi@gmail.com>
2018-03-05 12:20:16 +0330

    some changes

--- sheltie.py
+++ sheltie.py
@@ -21,22 +21,22 @@ def callback(ch, method, properties, body):
         #send_message(Failure(msg.id, message=str(e)))
 
 
-queue_name='command'
-credentials = pika.PlainCredentials(config.MQ_CONFIG['user'], config.MQ_CONFIG['pass'])
-parameters = pika.ConnectionParameters(host=config.MQ_CONFIG['host'],
-        port=config.MQ_CONFIG['port'],
-        virtual_host=config.MQ_CONFIG['vhost'],
-        credentials=credentials)
-connection = pika.BlockingConnection(parameters)
-channel = connection.channel()
-
-channel.queue_declare(queue=queue_name, durable=True)
-
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(callback,
-        queue=queue_name)
-
-channel.start_consuming()
-#if __name__ == '__main__':
-#    receiver(callback, config.QUEUE_CONFIG['command'])
+# queue_name='command'
+# credentials = pika.PlainCredentials(config.MQ_CONFIG['user'], config.MQ_CONFIG['pass'])
+# parameters = pika.ConnectionParameters(host=config.MQ_CONFIG['host'],
+#         port=config.MQ_CONFIG['port'],
+#         virtual_host=config.MQ_CONFIG['vhost'],
+#         credentials=credentials)
+# connection = pika.BlockingConnection(parameters)
+# channel = connection.channel()
+#
+# channel.queue_declare(queue=queue_name, durable=True)
+#
+# channel.basic_qos(prefetch_count=1)
+# channel.basic_consume(callback,
+#         queue=queue_name)
+#
+# channel.start_consuming()
+if __name__ == '__main__':
+    receiver(callback, config.QUEUE_CONFIG['command'])
 
